c38a946c6564dd07641aa197859e446a2a138408
Sachin Chaurasiya <122665616+chaurasiya-sachin@users.noreply.github.com>
2024-09-27 19:30:13 +0000

    types of variables in JS

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId=12457;
+let email="xachin@gmail.com"
+var password="12345" //not prefer to use var because of issue in the block scope
+city="patna" //not use without type 
+let state 
+
+// accountId=1 //not allowed
+
+email="sachin@gmail.com"
+password="0111"
+city="ara"
+
+
+// console.log(accounId);
+
+console.table([accountId,email,password,city,state]);
