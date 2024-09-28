89f3c3955e40af6352ef31fbe1060ab918c32625
Sachin Chaurasiya <122665616+chaurasiya-sachin@users.noreply.github.com>
2024-09-28 19:44:09 +0000

    learn data types in javaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //this treat all js as newer version
+
+// alert("hello") //this will not work in node , it will only work in browser
+
+console.log(3
+    +3
+) //don't use this its bad practice
+
+// data types 
+
+/*
+number => 1,2 .. range 2 to the power 53
+bigInt - to store bigger number
+string
+boolean 
+null 
+undefined
+symbols
+*/
+
+console.log(typeof null) // object data type
+console.log(typeof undefined) // undefined data type
